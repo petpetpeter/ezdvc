578c5b42b6286564c6d19e8f0da92eb2c060230c
petpetpeter <chana.pol.peter@gmail.com>
2022-10-21 13:41:27 +0900

    starting

---
+++ studentpredictor/train_model.py
@@ -0,0 +1,10 @@
+import pickle
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from config import Config
+Config.MODELS_PATH.mkdir(parents=True, exist_ok=True)
+X_train = pd.read_csv(str(Config.FEATURES_PATH / "train_features.csv"))
+y_train = pd.read_csv(str(Config.FEATURES_PATH / "train_labels.csv"))
+model = LinearRegression()
+model = model.fit(X_train, y_train.to_numpy().ravel())
+pickle.dump(model, open(str(Config.MODELS_PATH / "model.pickle"), "wb"))
